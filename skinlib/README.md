d19f80d95b449cd060de0347346ccea024e50dbc
zatrit <zatrit@gmail.com>
2023-09-05 19:31:07 +0300

    Replaced skinlib subproject with Jitpack dependency to keep skinlib unified for all branches

--- skinlib/README.md
+++
@@ -1,5 +0,0 @@
-# skinlib
-
-A simple Minecraft skin loading library that supports various APIs.
-Designed for OpenMCSkins portability because *skinlib* does not depend on
-Minecraft itself.
--- skinlib/src/main/java/net/zatrit/skins/lib/api/Layer.java
+++
@@ -1,9 +0,0 @@
-package net.zatrit.skins.lib.api;
-
-public interface Layer<T> {
-    T apply(T input);
-
-    default Layer<T> andThen(Layer<T> layer) {
-        return t -> layer.apply(apply(t));
-    }
-}
