78aecd80c41b5c90bed808c8da273cfc47037582
zatrit <zatrit@gmail.com>
2023-10-29 00:53:42 +0300

    Returned Mojang default config option, fixed wrong warning supress comment location

--- src/main/java/net/zatrit/skins/config/SkinsConfig.java
+++ src/main/java/net/zatrit/skins/config/SkinsConfig.java
@@ -17,5 +17,8 @@ public class SkinsConfig implements ConfigData {
     private boolean refreshOnConfigSave = true;
     private float loaderTimeout = 2f;
     private UuidMode uuidMode = UuidMode.OFFLINE;
-    private List<HostEntry> hosts = Lists.newArrayList(new HostEntry(HostEntry.HostType.FALLBACK));
+    private List<HostEntry> hosts = Lists.newArrayList(
+            new HostEntry(HostEntry.HostType.MOJANG),
+            new HostEntry(HostEntry.HostType.FALLBACK)
+    );
 }
