e3aba07a7c41ab97c8f0ccb35ba2296df56863c9
zatrit <zatrit@gmail.com>
2023-12-03 13:29:39 +0300

    Fixed omcs list crash

--- src/main/java/net/zatrit/skins/util/command/TextUtil.java
+++ src/main/java/net/zatrit/skins/util/command/TextUtil.java
@@ -15,6 +15,7 @@ import java.net.URL;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.UnaryOperator;
 
 /**
@@ -47,7 +48,7 @@ public class TextUtil {
         for (val entry : map.entrySet()) {
             val value = entry.getValue();
 
-            if (SKIP_ELEMENTS.stream().anyMatch(value::equals)) {
+            if (SKIP_ELEMENTS.stream().anyMatch(e -> Objects.equals(e, value))) {
                 continue;
             }
 
