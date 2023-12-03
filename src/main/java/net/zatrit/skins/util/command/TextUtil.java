6d678c05bf3c06e864213fb7a34e580817d6969a
zatrit <zatrit@gmail.com>
2023-12-03 12:50:42 +0300

    Check CHANGELOG.md

--- src/main/java/net/zatrit/skins/util/command/TextUtil.java
+++ src/main/java/net/zatrit/skins/util/command/TextUtil.java
@@ -15,7 +15,6 @@ import java.net.URL;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.function.UnaryOperator;
 
 /**
@@ -48,7 +47,7 @@ public class TextUtil {
         for (val entry : map.entrySet()) {
             val value = entry.getValue();
 
-            if (SKIP_ELEMENTS.stream().anyMatch(e -> Objects.equals(e, value))) {
+            if (SKIP_ELEMENTS.stream().anyMatch(value::equals)) {
                 continue;
             }
 
