358861d221c9ec64c743b4fb8051255f17e8bd41
zatrit <zatrit@gmail.com>
2024-07-03 11:22:29 +0300

    code: optimized GameProfileMixin

--- src/main/java/net/zatrit/skins/mixin/GameProfileMixin.java
+++ src/main/java/net/zatrit/skins/mixin/GameProfileMixin.java
@@ -12,15 +12,12 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URI;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.net.URL;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
-
-import static net.zatrit.skins.lib.util.SneakyLambda.sneaky;
 
 @Mixin(value = GameProfile.class, remap = false)
 public abstract class GameProfileMixin implements Profile, AsyncUUIDRefresher {
@@ -29,40 +26,28 @@ public abstract class GameProfileMixin implements Profile, AsyncUUIDRefresher {
 
     @Override
     public CompletableFuture<Profile> skins$refreshUuid() {
-        return CompletableFuture.supplyAsync(this::apiRequest)
-            .thenApply(request -> SkinsClient.getHttpClient()
-                .sendAsync(
-                    request,
-                    HttpResponse.BodyHandlers.ofInputStream()
-                ).join()).thenApply(
-                HttpResponse::body).thenApply(sneaky(stream -> {
-                @Cleanup val reader = new InputStreamReader(stream);
-                val map = SkinsClient.getSkinlibConfig().getGson().fromJson(
-                    reader,
-                    Map.class
-                );
+        return CompletableFuture.supplyAsync(() -> {
+            @Cleanup val reader = new InputStreamReader(apiRequest());
+            val map = SkinsClient.getSkinlibConfig().getGson().fromJson(reader,
+                                                                        Map.class
+            );
 
-                val id = String.valueOf(map.get("id")).replaceAll(
-                    "(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})",
-                    "$1-$2-$3-$4-$5"
-                );
+            val id = String.valueOf(map.get("id")).replaceAll("(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})",
+                                                              "$1-$2-$3-$4-$5"
+            );
 
-                val profile = new GameProfile(
-                    UUID.fromString(id),
-                    this.getName()
-                );
-                profile.getProperties().putAll(this.getProperties());
+            val profile = new GameProfile(UUID.fromString(id), this.getName());
+            profile.getProperties().putAll(this.getProperties());
 
-                return (Profile) profile;
-            }));
+            return (Profile) profile;
+        });
     }
 
     @Unique
     @SneakyThrows
-    private HttpRequest apiRequest() {
-        return HttpRequest.newBuilder().uri(new URI(
-            "https://api.mojang.com/users/profiles/minecraft/" +
-                this.getName())).build();
+    private InputStream apiRequest() {
+        return new URL("https://api.mojang.com/users/profiles/minecraft/" +
+                           this.getName()).openStream();
     }
 }
 
